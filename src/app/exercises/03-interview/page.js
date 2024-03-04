c1fc052eeecec8f4204c710cf4baa478cad0ddae
Simon Legg <leggsimon@gmail.com>
2024-03-04 14:58:16 +0100

    create client component to hydrate

--- src/app/exercises/03-interview/page.js
+++ src/app/exercises/03-interview/page.js
@@ -1,19 +1,14 @@
-'use client';
 import React from 'react';
-import { useMediaQuery } from 'react-responsive';
 
 import Interview from './Interview';
 import './styles.css';
+import ShowOnDesktop from './ShowOnDesktop';
 
 function InterviewExercise() {
-  const isDesktop = useMediaQuery({
-    query: '(min-width: 500px)',
-  });
-
   return (
     <main>
       <Interview />
-      {isDesktop && (
+      <ShowOnDesktop>
         <aside>
           <img
             src="/gwen-artist.png"
@@ -21,15 +16,16 @@ function InterviewExercise() {
           />
           <h2>About the Artist</h2>
           <p>
-            Gwen Altaria is a contemporary artist known for
-            her unique blend of traditional oil painting
-            techniques and pop culture references. Born and
-            raised in London, England, Vivi had an early
-            love for both the fine arts and the fantastical
-            worlds of video games.
+            Gwen Altaria is a contemporary artist
+            known for her unique blend of
+            traditional oil painting techniques and
+            pop culture references. Born and raised
+            in London, England, Vivi had an early
+            love for both the fine arts and the
+            fantastical worlds of video games.
           </p>
         </aside>
-      )}
+      </ShowOnDesktop>
     </main>
   );
 }
